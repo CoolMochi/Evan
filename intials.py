051a9efb49a7b2c8e3dad97b0c0e9c5998dc36db
CoolMochi <86621421+CoolMochi@users.noreply.github.com>
2021-07-01 12:55:05 -0700

    Create intials.py

---
+++ intials.py
@@ -0,0 +1,25 @@
+// g++ initials.cpp -o cwc.o
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "EEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE\n";
+	cout << "E                         E                        E             E\n";
+	cout << "E    						E						 E			   E\n";
+	cout << "E 						E						 E 			   E\n";
+	cout << "E  						E 						 E             E\n";
+	cout << "E							E						 E			   E\n";
+	cout << "E							E						 E 			   E\n";
+	cout << "E							E						 E 			   E\n";
+	cout << "EEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE\n";
+	cout << "E							E						 E			   E\n";
+	cout << "E 						E						 E 			   E\n";
+	cout << "E							E						 E			   E\n";
+	cout << "E							E				 		 E			   E\n";
+	cout << "E							E		   			     E			   E\n";
+	cout << "EEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE\n";
+	cout << "E							E  						 E			   E\n";
+
+	
+	return 0;
+}
